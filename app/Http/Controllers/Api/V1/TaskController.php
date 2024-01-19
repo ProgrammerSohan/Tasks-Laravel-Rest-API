786fcdeb26fbd690f887b890e9a8c66e4ab11792
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-01-19 10:12:27 +0600

    data deleting

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -71,6 +71,7 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        return response()->noContent();
     }
 }
